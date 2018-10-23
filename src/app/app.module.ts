c4bc7686efba4ff1d0fce223fcd45f40fc678b78
hristo2612 <hristoapps@gmail.com>
2018-10-23 17:31:04 +0300

    Update app.module - include necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DeveloperListComponent } from './developer-list/developer-list.component';
+import { SearchDevelopersPipe } from './search-developers.pipe';
+import { DeveloperDetailsComponent } from './developer-details/developer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeveloperListComponent,
+    SearchDevelopersPipe,
+    DeveloperDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
