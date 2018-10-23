f02b04d1625f2c310a168d252752901bfe3ad057
Hristo Bogoev <hristob@tor.pyrsoftware.com>
2018-10-23 15:25:40 +0300

    Update app.component - includes a search field with a list of developers to show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+interface IDeveloper {
+  name: string;
+  shortName: string;
+  reKnown: string;
+  bio: string;
+  highlight?: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'understand-angular';
+  query: string;
+  developers: IDeveloper[];
+
+  public showDeveloper(developer) {
+    this.query = developer.name;
+    developer.highlight = true;
+  }
+  constructor() {
+    this.query = '';
+    this.developers = [
+      {
+        name: 'Hristo Bogoev',
+        shortName: 'icko',
+        reKnown: 'Software University from outta space',
+        bio: 'Likes to learn and teach great stuff',
+      },
+      {
+        name: 'Linus Torvalds',
+        shortName: 'linus',
+        reKnown: 'Unknown',
+        bio: 'Writes linux things..',
+      },
+      {
+        name: 'Bai Ganio',
+        shortName: 'bai ganio',
+        reKnown: 'From MIT',
+        bio: 'Goes around and around and around..',
+      }
+    ];
+  }
 }
